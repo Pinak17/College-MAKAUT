f925c0038739ca42e8b28634e5f684bf56182a41
Pinak17 <pinak.roychowdhury@gmail.com>
2018-02-14 23:55:24 +0530

    Newton-Raphson

---
+++ 4th-sem/M(CS)491-numerical-method/newtonRaphson.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+double function(double);
+double function_derivative(double);
+
+int main(){
+	double i=0,x=0,f,f1,x1;
+	do{
+		f=function(x);
+		f1=function_derivative(x);
+		x1=x-(f/f1);
+		double d=fabs(x1-x);
+		if(d < 0.0005 && d>-0.0005){
+			break;
+		}
+		printf("The difference is %f\n", d);
+		x=x1;
+		i++;
+	}while(1 && i<50);
+	printf("The ans is %f\n",x1);
+	return 0;
+}
+
+double function(double x){
+	double exp;
+	exp = (x*x*x)+(3*x)-2;
+	return exp;
+}
+
+double function_derivative(double x){
+	double exp_d;
+	exp_d = (3*x*x)+3;
+	return exp_d;
+}
